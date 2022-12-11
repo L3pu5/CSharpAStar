d2a1034fe8487ebf70069830c710cb49870d87b1
Lepus <lepus@lepus.site>
2022-12-11 19:53:49 +1000

    Revert "Completed basic AStar Pathing in C#, graphical display in console works"
    This reverts commit f139917b169be8ce9771511cebb1cb551577630f.

--- Program.cs
+++ Program.cs
@@ -10,14 +10,7 @@ namespace CSharpAStar
     {
         static void Main(string[] args)
         {
-            Map testMap = new Map(15,6);
-            Graphics.DrawMap(testMap); 
-            AStar path = new AStar(testMap.GetTile(2, 3), testMap.GetTile(10, 1), testMap);
-            if(path.Path != null)
-                Graphics.DrawMap(testMap);
-            else
-                Console.WriteLine("Path failed!");
-            Graphics.DrawMap(testMap);
+            
         }
     }
 }
